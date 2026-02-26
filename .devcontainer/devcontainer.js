f4d2dfb0ad0490dcaa850b58b490757e61eeb2c1
brandonang242-sudo <brandonang242@gmail.com>
2026-02-26 21:41:29 +0800

    Delete .devcontainer directory

--- .devcontainer/devcontainer.js
+++
@@ -1,6 +0,0 @@
-{
-  "name": "React Native Dev Container",
-  "image": "mcr.microsoft.com/vscode/devcontainers/javascript-node:0-22",
-  "postCreateCommand": "npm install",
-  "remoteUser": "vscode"
-}
